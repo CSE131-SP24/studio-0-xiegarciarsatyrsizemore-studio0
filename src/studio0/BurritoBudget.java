22886d67a6d9fc317271ea01555390fccd55594f
braxtonsizemore <braxtonsizemore@localhost>
2024-01-17 15:45:44 -0600

    Here is my burrito budget!

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -9,7 +9,7 @@ public class BurritoBudget {
 		int burritosEatenWeek4 = 1;
 		
 		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
+		double averageWeeklyBurritos = totalBurritos / 4.0;
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
